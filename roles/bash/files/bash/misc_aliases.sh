7708a908154fbea2da03773599bd7a9c291c31f3
Andrew Gluck <andrew.gluck@prounlimited.com>
2024-01-09 17:46:23 -0500

    setting up automation for all sysetms in my local env

--- roles/bash/files/bash/misc_aliases.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-alias bt='bpytop'
-alias update='sudo nala update && sudo nala upgrade -y && sudo nala autoremove -y'
-alias comment-header='toilet -f pagga -S'
-alias i='explorer.exe'
